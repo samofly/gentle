46c003e11229605324463bb42dbcbfcfefbf2d31
Ivan Krasin <imkrasin@gmail.com>
2014-03-21 16:09:37 -0700

    Only accept gcode from the poorly-implemented whitelist. Currently, it's all commands which start with 'G'

--- gentle/gentle.go
+++ gentle/gentle.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/samofly/sers"
 )
@@ -22,6 +23,29 @@ func scan(s sers.SerialPort) {
 		log.Fatal("Failed to read from serial port:", err)
 	}
 	log.Fatal("Serial port closed")
+}
+
+// sanitizeG handle Gnn commands. cmd is upper-case, trimmed and starts with 'G'
+func sanitizeG(cmd string) (string, error) {
+	return cmd, nil
+}
+
+// sanitizeCmd checks gcode command and returns a canonically-formatted gcode without comments.
+func sanitizeCmd(cmd string) (string, error) {
+	cmd = strings.ToUpper(strings.TrimSpace(cmd))
+	if cmd == "" {
+		return "", nil
+	}
+
+	// TODO(krasin): implement a proper gcode parser.
+	switch cmd[0] {
+	case 'G':
+		return sanitizeG(cmd)
+	default:
+		return "", fmt.Errorf("sanitizeCmd(%q): %q command not recognized", cmd, cmd[0])
+	}
+
+	return cmd, nil
 }
 
 func main() {
@@ -44,7 +68,18 @@ func main() {
 	fmt.Fprintln(os.Stderr, "Please, enter g-code lines below:")
 	in := bufio.NewScanner(os.Stdin)
 	for in.Scan() {
-		cmd := fmt.Sprintf(`{"gc":"%s"}`, in.Text())
+		gcode, err := sanitizeCmd(in.Text())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid gcode: %v\n", err)
+			// Invalid / unrecognized gcode is a halting condition,
+			// because we don't know what's the supposed CNC state after this line,
+			// and it may hurt the part and the mill.
+			os.Exit(1)
+		}
+		if gcode == "" {
+			continue
+		}
+		cmd := fmt.Sprintf(`{"gc":"%s"}`, gcode)
 		fmt.Println(cmd)
 		if _, err := fmt.Fprintln(s, cmd); err != nil {
 			log.Fatal("Failed to write to serial port:", err)
