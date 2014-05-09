3895160529cbac8f2ce737dd8b4229fd5741b55f
Ivan Krasin <imkrasin@gmail.com>
2014-05-08 20:25:39 -0700

    Add tools/websocket-client to test gentle WebSockets server.

---
+++ tools/websocket-client/main.go
@@ -0,0 +1,53 @@
+// websocket-client is a command line utility to connect to gentle server via WebSockets.
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"code.google.com/p/go.net/websocket"
+)
+
+var url = flag.String("url", "ws://echo.websocket.org:80", "URL to connect to")
+var origin = flag.String("origin", "http://localhost/", "Origin URL to send to server")
+
+func read(r io.Reader) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(string(buf[:n]))
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *url == "" {
+		log.Fatal("-url not specified")
+	}
+
+	ws, err := websocket.Dial(*url, "", *origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	go read(ws)
+
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		if _, err := ws.Write(s.Bytes()); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
