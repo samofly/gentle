94c5e702f745dbd01919a483de5152ccdd1dfd4e
Ivan Krasin <imkrasin@gmail.com>
2014-05-31 23:56:13 +0000

    bootstrap: add files to gen.sh and regenerate bindata.go

--- gentle/gen.sh
+++ gentle/gen.sh
@@ -12,5 +12,9 @@ die() {
 ( go-bindata -version ) || die "Could not find go-bindata. Please, install it with:\n\
   go get github.com/samofly/go-bindata/go-bindata"
 
-go-bindata -ignore '.*~'  -prefix 'web' ./web ./web/js ./web/css
+go-bindata -ignore '.*~'  -prefix 'web' ./web ./web/js ./web/css \
+./web/third_party/bootstrap/css \
+./web/third_party/bootstrap/fonts \
+./web/third_party/bootstrap/js
+
 gofmt -w bindata.go
